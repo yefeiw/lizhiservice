aa0d07bc1d133e0938544c3b80202c828fd9b150
Yefei Wang <yefeiw@Yefeis-MacBook-Pro.local>
2017-04-30 21:51:18 -0700

    sorted list of beikao

--- auto_classify_and_recommend.py
+++ auto_classify_and_recommend.py
@@ -9,6 +9,11 @@
 import time
 import random
 import json
+#error logging
+import logging
+import traceback
+#email utilities
+import smtplib
 
 from selenium import webdriver
 #home url: Homepage of Beikao
@@ -16,17 +21,23 @@ home_url = 'http://lizhi.fm/40624'
 output_file = 'dump_output.txt'
 #setup proxy
 #chrome_options = webdriver.ChromeOptions()
-
-driver = webdriver.Chrome()   
-driver.get(home_url)
-#click audio play button and wait for 30 seconds
-# this is to ensure that that the audio will play properly
-xpath_titles = driver.find_elements_by_xpath('//div/p[contains(@class,"audioName")]')
-xpath_descriptions = driver.find_elements_by_xpath('//div[contains(@class,"audio-desc")]')
-import pdb; pdb.set_trace()
-#output to file for storage for now
-with open(output_file, 'w') as f:
-	f.write(json.dumps(xpath_titles))
-	#f.write(json.dumps(xpath_descriptions))
-driver.quit()
+try:
+	driver = webdriver.Chrome()   
+	driver.get(home_url)
+	#get the titles for each file
+	xpath_titles = driver.find_elements_by_xpath('//div/p[contains(@class,"audioName")]')
+	xpath_descriptions = driver.find_elements_by_xpath('//div[contains(@class,"audio-desc")]')
+	import pdb; pdb.set_trace()
+	#output to file for storage for now
+	with open(output_file, 'w') as f:
+		#f.write(json.dumps(xpath_titles))
+		#f.write(json.dumps(xpath_descriptions))
+		for xpath_title in xpath_titles:
+			f.write(xpath_title.text)
+			f.write('\n')	
+		f.close()
+except:
+		logging.error(traceback.print_exc())
+finally:
+	driver.close();
  
