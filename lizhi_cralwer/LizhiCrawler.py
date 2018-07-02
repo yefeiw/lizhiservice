a911fccb257533798c158f13d06e0589a4e0e356
yefeiw <jeffxanthus@gmail.com>
2018-07-02 11:41:43 -0700

    Optimized using PyCharm

--- lizhi_cralwer/LizhiCrawler.py
+++ lizhi_cralwer/LizhiCrawler.py
@@ -1,144 +1,144 @@
-###Name: LizhiCrawler.py
-### Iteratively categorize each lizhi post for Beikao and recommend what should be recorded next
+# Name: LizhiCrawler.py
+# Iteratively categorize each lizhi post for Beikao and recommend what should be recorded next
 
-### Dependencies:
+# Dependencies:
 
 #   selenium: (pip install -U selenium)
 #   relevant webdriver 
 
-import time
-import random
-import json
-#error logging
 import logging
-import traceback
-#email utilities
-import smtplib
 import re
-import matplotlib.pyplot as plt
-###cross file checks
+import time
+import traceback
 
-from selenium import webdriver
-#home url: Homepage of Beikao
+# home url: Homepage of Beikao
 home_url = 'http://lizhi.fm/40624'
 output_file = '../dump_output.txt'
-###util functions
+
+
+# util functions
+
+
 def if_xpath_exist(driver, xpath):
-	test = len(driver.find_elements_by_xpath(xpath))
-	if test > 0:
-		return True
-	else:
-		return False
+    test = len(driver.find_elements_by_xpath(xpath))
+    if test > 0:
+        return True
+    else:
+        return False
 
-def get_category(title,desc,types):
-	for item in types:
-		if (re.search(item,title)):
-			return item
-		if (re.search(item,desc)):
-			return item
-	return "beikao default"
 
-##class declaration reserved for future use
+def get_category(title, desc, types):
+    for item in types:
+        if (re.search(item, title)):
+            return item
+        if (re.search(item, desc)):
+            return item
+    return "beikao default"
+
+
+# class declaration reserved for future use
+
+
 class LizhiStat:
-	def __init__(self):
-		self.title = ""
-		self.description=""
-		self.viewed = 0
-		self.hosts = []
-		self.visitors = []
+    def __init__(self):
+        self.title = ""
+        self.description = ""
+        self.viewed = 0
+        self.hosts = []
+        self.visitors = []
 
 
-		
-
-####################Structural Functions####################
 class LizhiCrawler:
-	def __init__(self,types,hosts):
-		self.types = types
-		self.hosts = hosts
-		self.categories = dict()
-		self.hostcount = dict()
-		self.recentshow = dict()
-		return
-	def add_hosts(self,desc,timestamp):
-		lines = desc.text.split('\n')
-		for line in lines:
-			#import pdb; pdb.set_trace()
-			if re.search("本期主播：",line):
-				for host in self.hosts:
-					if re.search(host,line):
-						print("found host"+host+"\n")
-						if self.hostcount[host] is 0:
-							self.recentshow[host] = timestamp.text
-						self.hostcount[host] +=1
-	def get_categories(self):
-		return self.categories
-	def get_hostcount(self):
-		return self.hostcount
-	def get_recentshow(self):
-		return self.recentshow
-	def retrieve_hrefs(self,driver):
-		audio_list_addr = '//ul[contains(@class,"js-audio-list")]'
-		next_page_addr = '//a[contains(@class,"next")]'
-		hrefs = []
-		try:
+    def __init__(self, types, hosts):
+        self.types = types
+        self.hosts = hosts
+        self.categories = dict()
+        self.hostcount = dict()
+        self.recentshow = dict()
+        return
 
-			driver.get(home_url)
-		  
-			#find the next page button
-			#long version for full functionality
-			while if_xpath_exist(driver,audio_list_addr):
-				 #get the refs for each file
-				xpath_audiolist= driver.find_element_by_xpath(audio_list_addr)
-				xpath_lists = xpath_audiolist.find_elements_by_xpath('li//a[contains(@class,"audio-list-item")]')
-				for xpath_item in xpath_lists:
-					try:
-						ref = xpath_item.get_attribute('href')
-						hrefs.append(ref)
-					except:
-						logging.error(traceback.print_exc())
-				if if_xpath_exist(driver,next_page_addr):
-					#test: break anyways
-					#break
-					#mainline: go to next page
-					next_page = driver.find_element_by_xpath(next_page_addr)
-					next_page.click()
-				else:
-					break
-			
-		except:
-				logging.error(traceback.print_exc())
-		finally:
-			return hrefs
+    def add_hosts(self, desc, timestamp):
+        lines = desc.text.split('\n')
+        for line in lines:
+            # import pdb; pdb.set_trace()
+            if re.search("本期主播：", line):
+                for host in self.hosts:
+                    if re.search(host, line):
+                        print("found host" + host + "\n")
+                        if self.hostcount[host] is 0:
+                            self.recentshow[host] = timestamp.text
+                        self.hostcount[host] += 1
 
-	def process(self,driver, hrefs):
-	
-		title_xpath = ('//h1[contains(@class,"audioName")]')
-		desc_xpath  =('//div[contains(@class,"desText")]')
-		timestamp_xpath = ('//span[contains(@class,"audioTime")]')
-		for item in self.types:
-			self.categories[item] = []
-		for person in self.hosts:
-			self.hostcount[person] = 0
-			self.recentshow[person] = ""
-		try:
-			for href in hrefs:
-				print ("href is "+href)
-				driver.get(href)
-				time.sleep(1)
-				if  (if_xpath_exist(driver,title_xpath) is False) or (if_xpath_exist(driver,desc_xpath) is False):
-					print ("this href postition "+href+" is not valid, please manually verify later")
-					continue
-				title = driver.find_element_by_xpath(title_xpath)
-				desc = driver.find_element_by_xpath(desc_xpath)
-				timestamp = driver.find_element_by_xpath(timestamp_xpath)
-				#import pdb;pdb.set_trace()
-				category = get_category(title.text,desc.text,self.types)
-				self.add_hosts(desc,timestamp)
-				self.categories[category].append(title.text)
-				print(category+": current count " + str(len(self.categories[category])))
-		except:
-			logging.error(traceback.print_exc())
-		finally:
-			pass
+    def get_categories(self):
+        return self.categories
 
+    def get_hostcount(self):
+        return self.hostcount
 
+    def get_recentshow(self):
+        return self.recentshow
+
+    def retrieve_hrefs(self, driver):
+        audio_list_addr = '//ul[contains(@class,"js-audio-list")]'
+        next_page_addr = '//a[contains(@class,"next")]'
+        hrefs = []
+        try:
+
+            driver.get(home_url)
+
+            # find the next page button
+            # long version for full functionality
+            while if_xpath_exist(driver, audio_list_addr):
+                # get the refs for each file
+                xpath_audiolist = driver.find_element_by_xpath(audio_list_addr)
+                xpath_lists = xpath_audiolist.find_elements_by_xpath('li//a[contains(@class,"audio-list-item")]')
+                for xpath_item in xpath_lists:
+                    try:
+                        ref = xpath_item.get_attribute('href')
+                        hrefs.append(ref)
+                    except:
+                        logging.error(traceback.print_exc())
+                if if_xpath_exist(driver, next_page_addr):
+                    # test: break anyways
+                    # break
+                    # mainline: go to next page
+                    next_page = driver.find_element_by_xpath(next_page_addr)
+                    next_page.click()
+                else:
+                    break
+
+        except:
+            logging.error(traceback.print_exc())
+        finally:
+            return hrefs
+
+    def process(self, driver, hrefs):
+
+        title_xpath = '//h1[contains(@class,"audioName")]'
+        desc_xpath = '//div[contains(@class,"desText")]'
+        timestamp_xpath = '//span[contains(@class,"audioTime")]'
+        for item in self.types:
+            self.categories[item] = []
+        for person in self.hosts:
+            self.hostcount[person] = 0
+            self.recentshow[person] = ""
+        try:
+            for href in hrefs:
+                print("href is " + href)
+                driver.get(href)
+                time.sleep(1)
+                if (if_xpath_exist(driver, title_xpath) is False) or (if_xpath_exist(driver, desc_xpath) is False):
+                    print("this href postition " + href + " is not valid, please manually verify later")
+                    continue
+                title = driver.find_element_by_xpath(title_xpath)
+                desc = driver.find_element_by_xpath(desc_xpath)
+                timestamp = driver.find_element_by_xpath(timestamp_xpath)
+                # import pdb;pdb.set_trace()
+                category = get_category(title.text, desc.text, self.types)
+                self.add_hosts(desc, timestamp)
+                self.categories[category].append(title.text)
+                print(category + ": current count " + str(len(self.categories[category])))
+        except:
+            logging.error(traceback.print_exc())
+        finally:
+            pass
--- lizhi_cralwer/LizhiServiceMain.py
+++ lizhi_cralwer/LizhiServiceMain.py
@@ -1,52 +1,47 @@
-import time
-import random
-import json
-#error logging
+# error logging
 import logging
 import traceback
-#email utilities
-import smtplib
-import re
 
 ###cross file checks
 import LizhiCrawler
 import LizhiMail
 import LizhiSoup
-
 from selenium import webdriver
-types = ['吐槽','互撕','吃喝玩乐','嘉宾','特别','热点','在美国','beikao default']
-hosts = ['Ada','丫头','Ruby','豆包','老王','黑人','奔','阿瓜','小哈','小龙哥','九月','大白','波波']
 
-crawler = LizhiCrawler.LizhiCrawler(types,hosts)
-soup = LizhiSoup.LizhiSoup('./email-inlined.html','./category.txt','./email-output.html')
+# email utilities
+
+types = ['吐槽', '互撕', '吃喝玩乐', '嘉宾', '特别', '热点', '在美国', 'beikao default']
+hosts = ['Ada', '丫头', 'Ruby', '豆包', '老王', '黑人', '奔', '阿瓜', '小哈', '小龙哥', '九月', '大白', '波波']
+
+crawler = LizhiCrawler.LizhiCrawler(types, hosts)
+soup = LizhiSoup.LizhiSoup('./email-inlined.html', './category.txt', './email-output.html')
 mail = LizhiMail.LizhiMail()
-
 
 output_file = "./category.txt"
 try:
-	driver = webdriver.PhantomJS()
-	addresses = crawler.retrieve_hrefs(driver)
-	crawler.process(driver, addresses)
-	categories = crawler.get_categories()
-	hostcount = crawler.get_hostcount()
-	recentshow = crawler.get_recentshow()
-	#output to file for storage for now
-	with open(output_file, 'w') as f:
-		for item in types:
-			f.write('\n\n\ntitle#'+item+'\n')
-			items = categories[item];
-			f.write("total length: %d\n" % len(items))
-		for person in hosts:
-			f.write('host#'+person+": \n")
-			f.write("\ttotal recordings: "+ str(hostcount[person])+'\n')
-			f.write("\tmost recent appearance " + recentshow[person]+'\n')
-		f.close()
+    driver = webdriver.PhantomJS()
+    addresses = crawler.retrieve_hrefs(driver)
+    crawler.process(driver, addresses)
+    categories = crawler.get_categories()
+    hostcount = crawler.get_hostcount()
+    recentshow = crawler.get_recentshow()
+    # output to file for storage for now
+    with open(output_file, 'w') as f:
+        for item in types:
+            f.write('\n\n\ntitle#' + item + '\n')
+            items = categories[item];
+            f.write("total length: %d\n" % len(items))
+        for person in hosts:
+            f.write('host#' + person + ": \n")
+            f.write("\ttotal recordings: " + str(hostcount[person]) + '\n')
+            f.write("\tmost recent appearance " + recentshow[person] + '\n')
+        f.close()
 
-	soup.parse()
-	mail.sendMail()
+    soup.parse()
+    mail.sendMail()
 
-	
+
 except:
-	logging.error(traceback.print_exc())
+    logging.error(traceback.print_exc())
 finally:
-	driver.close()
+    driver.close()
